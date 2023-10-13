308a29233a7fec4930c6ea3bcb3c3e0696d60d1f
richardnidhog <richardnidhogzhou@gmail.com>
2023-10-12 20:42:53 -0500

    Creat by XZ

--- CHI_Tiktok_test1.py
+++ CHI_Tiktok_test1.py
@@ -7,7 +7,7 @@ import getpass
 
 openai.api_key = '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'
 
-text = input("Please enter a piece of text: ")
+text = input("Please enter the text you want to check: ")
 
 chat_prompt =   'Determine if the statement above contains any misinformation. In the part one, the results are first displayed in the first line, \'May contain misinformation\', \'Cannot be recognized at this time\' or \'No misinformation detected\'. In the part two, an operation is performed to extract keywords from the input text, providing up to six keywords, sorted in order of criticality. In the part three, briefly summarize the reasons for determining whether it contains misinformation. Three reasons of no more than 50 words each are required. Add line breaks between each section. Regardless of the state of the text given, it must be answered in the format given above.'
 
@@ -25,3 +25,5 @@ def call_gpt_model(prompt):
 answer = call_gpt_model(chat_prompt)
 
 print(answer)
+
+input('\nPress any key to exit')
